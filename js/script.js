3bacec2443481d79f7313f6e75c13c9639f7e53a
Czarek6688 <czarekrosloniec2006@gmail.com>
2026-06-17 11:16:12 +0200

    suma-iloczyn

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const dodajLiczby = function (
+  liczba1,
+  liczba2,
+  liczba3,
+  liczba4,
+  liczba5,
+  liczba6,
+) {
+  let wynik = liczba1 + liczba2 + liczba3 + liczba4 + liczba5 + liczba6;
+  console.log(wynik);
+};
+
+let liczbaPierwsza = 1;
+let liczbaDruga = 2;
+let liczbaTrzecia = 3;
+let liczbaCzwarta = 4;
+let liczbaPiata = 5;
+let liczbaSzosta = 6;
+
+dodajLiczby(
+  liczbaPierwsza,
+  liczbaDruga,
+  liczbaTrzecia,
+  liczbaCzwarta,
+  liczbaPiata,
+  liczbaSzosta,
+);
+
+const mnozenieLiczb = function (
+  liczba1,
+  liczba2,
+  liczba3,
+  liczba4,
+  liczba5,
+  liczba6,
+) {
+  let wynik = liczba1 * liczba2 * liczba3 * liczba4 * liczba5 * liczba6;
+  console.log(wynik);
+};
+
+let liczba1 = 1;
+let liczba2 = 2;
+let liczba3 = 3;
+let liczba4 = 4;
+let liczba5 = 5;
+let liczba6 = 6;
+
+mnozenieLiczb(
+  liczbaPierwsza,
+  liczbaDruga,
+  liczbaTrzecia,
+  liczbaCzwarta,
+  liczbaPiata,
+  liczbaSzosta,
+);

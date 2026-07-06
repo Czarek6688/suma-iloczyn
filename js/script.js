b3aa01d45d32910cab8ab35e9d3d1a1bd4ff7352
Czarek6688 <czarekrosloniec2006@gmail.com>
2026-07-06 09:04:29 +0200

    poprawka

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@ function oblicz(tablica) {
   let suma = 0;
   let iloczyn = 1;
 
-  suma = 1 + 2 + 3 + 4 + 5 + 6;
-
-  iloczyn = 1 * 2 * 3 * 4 * 5 * 6;
-
+  for (let i = 0; i < tablica.length; i++) {
+    suma += tablica[i];
+    iloczyn *= tablica[i];
+  }
   console.log("Suma: " + suma);
   console.log("Iloczyn: " + iloczyn);
 }
